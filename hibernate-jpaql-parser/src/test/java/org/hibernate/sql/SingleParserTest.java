461700ef33aca27ab6f1c82fc2d0dee06a06e152
Sanne Grinovero <sanne@hibernate.org>
2012-07-13 22:25:20 +0100

    New test helpers

---
+++ hibernate-jpaql-parser/src/test/java/org/hibernate/sql/SingleParserTest.java
@@ -0,0 +1,52 @@
+/* 
+ * Hibernate, Relational Persistence for Idiomatic Java
+ * 
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.hibernate.sql;
+
+import org.antlr.runtime.ANTLRStringStream;
+import org.antlr.runtime.CommonTokenStream;
+import org.antlr.runtime.RecognitionException;
+import org.antlr.runtime.tree.CommonTree;
+import org.hibernate.sql.ast.origin.hql.parse.HQLLexer;
+import org.hibernate.sql.ast.origin.hql.parse.HQLParser;
+import org.hibernate.sql.ast.origin.hql.parse.HQLParser.statement_return;
+
+/**
+ * @author Sanne Grinovero <sanne@hibernate.org> (C) 2012 Red Hat Inc.
+ */
+public class SingleParserTest {
+
+	public static void main(String[] args) throws RecognitionException {
+		String testCase = "from Zoo ha where ha.mammals[:name] = :id";
+		ANTLRStringStream antlrStream = new ANTLRStringStream( testCase );
+		HQLLexer lexer = new HQLLexer( antlrStream );
+		CommonTokenStream tokens = new CommonTokenStream( lexer );
+		showTokens( tokens );
+		HQLParser parser = new HQLParser( tokens );
+		statement_return statement = parser.statement();
+		showTokens( tokens );
+		CommonTree tree = (CommonTree) statement.getTree();
+		System.out.println( tree.toStringTree() );
+	}
+
+	private static void showTokens(CommonTokenStream tokens) {
+		System.out.println( tokens.getTokens() );
+	}
+}
